9654607c8b53dec7535289e764acc31c10128617
Anshuman212 <raianshuman201282@gmail.com>
2023-07-23 22:15:46 +0530

    login signup page setup

---
+++ task-manager/src/app/signup/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from 'react';
+import '../styles/globals.css';
+
+
+const SignUpPage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-dark">
+        <title>Sign Up</title>
+      <div className="w-full max-w-md bg-dark p-8 rounded-lg shadow-lg">
+        <h1 className="text-2xl text-primary font-semibold mb-6">Sign Up</h1>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-text font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-1 block w-full px-4 py-3 border rounded-lg bg-dark text-text focus:outline-none focus:ring-primary focus:border-primary"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-text font-medium">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-1 block w-full px-4 py-3 border rounded-lg bg-dark text-text focus:outline-none focus:ring-primary focus:border-primary"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-4 py-2 rounded-lg font-semibold bg-primary text-text hover:bg-secondary focus:outline-none focus:bg-secondary"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
